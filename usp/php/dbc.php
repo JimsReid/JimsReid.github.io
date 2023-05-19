be52d254f39373617a5a3e4510d28d1cdffdaa69
JimsReid <jimbrillantes21@gmail.com>
2023-05-20 00:16:35 +0800

    removed

--- usp/php/dbc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServerName = "db.uap.asia";
-$dbUsername = "sserv";
-$dbPassword = "06a1c35f319428ee5764f2af54fb3d07";
-$dbName = "sserv";
-
-$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
